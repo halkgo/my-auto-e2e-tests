015cdfef98aeb45feb141c36cab09adfa3b8959e
vasya <halkgo602@gmail.com>
2020-06-18 14:23:55 +0300

    check bugs with version

--- tests/drop_test.js
+++ tests/drop_test.js
@@ -2,13 +2,13 @@
 Feature('dropDown');
 
 
-Scenario.skip('Выбираем опцию Option 1',  (I) => {
+Scenario('Выбираем опцию Option 1',  (I) => {
     I.amOnPage('https://the-internet.herokuapp.com/dropdown');
     I.selectOption('#dropdown', 'Option 1' );
-    I.waitForValue('#dropdown', '1')
+    I.waitForValue({css:'#dropdown'}, '1')
 });
 
-Scenario('Выбираем опцию Option 2 ',  (I) => {
+Scenario.skip('Выбираем опцию Option 2 ',  (I) => {
     I.amOnPage('https://the-internet.herokuapp.com/dropdown');
     I.selectOption('#dropdown', '2' );
     I.seeTextEquals('Option 2',{ xpath: '//option[@selected]' });
